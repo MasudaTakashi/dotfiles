fd9a9513810d64e67e650aefd9ab41fa36e112f5
MasudaTakashi <takashi.masuda.1202.gae@gmail.com>
2016-06-19 23:51:56 +0900

    zsh add dotpath

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -2,7 +2,10 @@
 # PATH Settings
 # ------------------------------
 
+#export DOTPATH="${0:A:t}"
+export DOTPATH="$HOME/dotfiles"
+
 ### pyenv
-export PYENV_ROOT="$HOME/.pyenv
-export PATH="$PYENV_ROOT/bin:$PATH
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
